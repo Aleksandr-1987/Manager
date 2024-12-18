6aeae784c65e190e25ebb19f8ec3bae6c52e41bd
Aleksandr-1987 <aleksander13@ukr.net>
2024-12-18 20:18:38 +0200

    111

--- pages/main.php
+++ pages/main.php
@@ -1,27 +1,29 @@
 <div class="main">
-        <div class="container">
-            <div class="main_block">
-                <div class="main_start">
+    <div class="container">
+        <div class="main_block">
+            <div class="main_start">
                 <div class="main_start_title">
-                    Стань Лідером Сьогодні!
+                    Стань Лідером Сьогодні!!!
                 </div>
                 <div class="main_start_text">
-                    Відкрийте свій потенціал і навчіться вести за собою команду до успіху. Наші курси допоможуть вам здобути навички, які змінять ваше лідерство на краще. Підніміть свою кар’єру на новий рівень разом із нами!
+                    Відкрийте свій потенціал і навчіться вести за собою команду до успіху. Наші курси допоможуть вам
+                    здобути навички, які змінять ваше лідерство на краще. Підніміть свою кар’єру на новий рівень разом
+                    із нами!
                 </div>
                 <div class="main_start_btns">
                     <div class="main_start_btns_item">Актуальні</div>
                     <div class="main_start_btns_item main_orange">Усі курси</div>
                 </div>
-                </div>
-                <div class="main_end">
-                    <img class="main_end_img" src="./assets/img/intro.png" alt="">
-                    <div class="main_end_btns">
-                        <div class="main_end_btns_item btn_active"></div> 
-                        <div class="main_end_btns_item"></div>
-                        <div class="main_end_btns_item"></div>  
-                    </div>
-                    <img class="main_end_img_arrow" src="./assets/img/arrow.png" alt="" class="main_end_img_arrow">
-                </div>
             </div>
-        </div>        
+            <div class="main_end">
+                <img class="main_end_img" src="./assets/img/intro.png" alt="">
+                <div class="main_end_btns">
+                    <div class="main_end_btns_item btn_active"></div>
+                    <div class="main_end_btns_item"></div>
+                    <div class="main_end_btns_item"></div>
+                </div>
+                <img class="main_end_img_arrow" src="./assets/img/arrow.png" alt="" class="main_end_img_arrow">
+            </div>
+        </div>
     </div>
+</div>
